f2e69c742a18de3ce558dc7851dc888bf428e572
massingf <f.massing@campus.tu-berlin.de>
2021-12-20 13:08:23 +0100

    Added some comments, fixed computation of entropy according to stabilization.py.

--- scripts/ev_validation.py
+++ scripts/ev_validation.py
@@ -6,6 +6,9 @@ import matplotlib.pyplot as plt
 dlfn.parameters['form_compiler']['no-evaluate_basis_derivatives'] = False
 
 def maximum(func, cell_i, dim):
+    """
+    Return maximum of function func on cell cell_i. Only for validation of stabilization.py, won't work in parallel.
+    """
     V = func.function_space()
 
     dmap = np.reshape(cell_i.get_coordinate_dofs(), (-1, dim))
@@ -15,38 +18,41 @@ def maximum(func, cell_i, dim):
     return np.max(np.abs(func.vector().get_local()[idx]))
 
 def ev(J_n, J_n_, J_n_n, v, dt, h_, mesh_):
-    nu = .25 / dt * (3 * J_n**2 - 4 * J_n_**2 + J_n_n**2) + dlfn.dot(dlfn.grad(.5 * J_n**2), v)
+    """
+    Computes the continuous entropy residual for the special case
+        * c_e = 1.
+        * Nu_max -> infinity
+
+    This is only suitable if E(J)=.5 J^2 is of the same polynomial degree as the function space of J.
+    """
+    nu = .25 / dt * (3 * dlfn.inner(J_n, J_n) - 4 * dlfn.inner(J_n_, J_n_) + dlfn.inner(J_n_n, J_n_n))\
+         + dlfn.inner(J_n, dlfn.dot(dlfn.grad(J_n), v))
     return dlfn.project(h_ ** 2 * nu, dlfn.FunctionSpace(mesh_, 'CG', 2))
 
 #mesh = dlfn.UnitCubeMesh(5, 10, 20)
 x_0 = np.array([0., 0., 0.])
-x_1 = np.array([.0125, .0125, .0125])
-nx, ny, nz = 1, 1, 1
+x_1 = np.array([1., 1., 1.])
+nx, ny, nz = 5, 5, 5
 
 meshes = [dlfn.IntervalMesh(nx, x_0[0], x_1[0]),
-        dlfn.RectangleMesh(dlfn.Point(x_0[:2]), dlfn.Point(x_1[:2]), nx, ny),
-        dlfn.BoxMesh(dlfn.Point(x_0), dlfn.Point(x_1), nx, ny, nz)]
+          dlfn.RectangleMesh(dlfn.Point(x_0[:2]), dlfn.Point(x_1[:2]), nx, ny),
+          dlfn.BoxMesh(dlfn.Point(x_0), dlfn.Point(x_1), nx, ny, nz)]
 
 x_diff = x_1 - x_0
 h_min = [x_diff[0] / nx,
          min(x_diff[0] / nx, x_diff[1] / ny),
          min(x_diff[0] / nx, x_diff[1] / ny, x_diff[2] / nz)]
 
-Nu_max = 150.
+c_max = 10000000.
 
 # Expressions
 # Higher degree expressions for J lead to lower accuracy, probably due to approximation error in projection
-J_expr = [dlfn.Expression('x[0]', degree=2),
-          dlfn.Expression('x[0] + x[1]', degree=2),
-          dlfn.Expression('x[0] + x[1] + x[2]', degree=2)]
+J_expr = [dlfn.Expression((('x[0]',),), degree=2),
+          dlfn.Expression((('x[0]', 'x[0] - x[1]'), ('x[0] - x[1]', 'x[1]')), degree=2),
+          dlfn.Expression((('x[0]', 'x[0] - x[1]', 'x[2] + x[0]'),
+                           ('x[0] - x[1]', 'x[1]', 'x[2] + x[1] + x[0]'),
+                           ('x[2] + x[0]', 'x[2] + x[1] + x[0]', 'x[2]')), degree=2)]
 
-J_old_expr = [dlfn.Expression('x[0]', degree=1),
-              dlfn.Expression('x[0] + x[1]', degree=1),
-              dlfn.Expression('x[0] + x[1] + x[2]', degree=1)]
-
-J_old_old_expr = [dlfn.Expression('-x[0]', degree=1),
-                  dlfn.Expression('-x[1]', degree=1),
-                  dlfn.Expression('-x[2]', degree=1)]
 
 beta_const_expr = [dlfn.Expression(('1.',), degree=1),
                    dlfn.Expression(('1.', '1.'), degree=1),
@@ -56,15 +62,15 @@ beta_linear_expr = [dlfn.Expression(('2.',), degree=1),
                     dlfn.Expression(('x[0]', '-x[1]'), degree=1),
                     dlfn.Expression(('x[0]', 'x[1]', '-2. * x[2]'), degree=1)]
 
-coords_expr = [dlfn.Expression('x[0]', degree=1),
-               dlfn.Expression('x[0] + x[1]', degree=1),
-               dlfn.Expression('x[0] + x[1] + x[2]', degree=1)]
+coords_expr = [dlfn.Expression((('x[0]',),), degree=1),
+               dlfn.Expression((('x[0] + x[1]', 0.), (0., 0.)), degree=1),
+               dlfn.Expression((('x[0] + x[1] + x[2]', 0., 0.), (0., 0., 0.), (0., 0., 0.)), degree=1)]
 
 # EV-parameters
-E_norm = dlfn.Constant(1.)
+E_norm = dlfn.Constant(1.)  # Given normalized entropy variation
 c_e = 1.
-dt = dlfn.Constant(.25)
-t_step = dlfn.Constant(10)
+dt = dlfn.Constant(.25)  # time step size
+time_step = dlfn.Constant(10)  # pass time_step > 1 to compute entropy-viscosity with second order backwards differences
 
 degree = 2
 
@@ -72,81 +78,74 @@ degree = 2
 median_arr = np.zeros((3, 4))
 var_arr = np.zeros((3, 4))
 
-# TODO: Test for higher-rank J
 for i, mesh in enumerate(meshes):
-    dim = i + 1
+    dim = mesh.geometric_dimension()
     c = mesh.ufl_cell()
 
 
     Vh = dlfn.VectorFunctionSpace(mesh, 'DG', 1)
 
-    Jh = dlfn.FunctionSpace(mesh, 'CG', degree)
+    Jh = dlfn.TensorFunctionSpace(mesh, 'CG', degree)
 
     Jh_test = dlfn.FunctionSpace(mesh, 'DG', 0)
 
     n_dofs = Jh.dim()
 
+    # Zero and one tensor (e_i \otimes e_j)
+    zero = dlfn.Constant([[dlfn.Constant(0.) for _ in range(dim)] for __ in range(dim)])
 
-    # Zero and one
-    zero_scalar = dlfn.interpolate(dlfn.Constant(0.), Jh)
-    one_scalar = dlfn.interpolate(dlfn.Constant(1.), Jh)
+    zero_tensor = dlfn.interpolate(dlfn.as_tensor(zero), Jh)
 
     # Projections of Expressions
     beta_const = dlfn.project(beta_const_expr[i], Vh)
     beta_linear = dlfn.project(beta_const_expr[i], Vh)
 
-    coords = dlfn.project(coords_expr[i], Jh)
+    coords = dlfn.interpolate(coords_expr[i], Jh)
 
     J = dlfn.project(J_expr[i], Jh)
-    J_old = dlfn.project(J_old_expr[i], Jh)
-    J_old_old = dlfn.project(J_old_old_expr[i], Jh)
-
+    J_old = dlfn.project(J_expr[i], Jh)
+    J_old_old = dlfn.project(J_expr[i], Jh)
 
     # Analytical solutions
-    Nu_J_projected = ev(J, zero_scalar, zero_scalar, beta_const, dt, h_min[i], mesh)
-    Nu_J_old_projected = ev(zero_scalar, J_old, zero_scalar, beta_const, dt, h_min[i], mesh)
-    Nu_J_old_old_projected = ev(zero_scalar, zero_scalar, J_old_old, beta_const, dt, h_min[i], mesh)
-
-    Nu_beta_projected = ev(coords, zero_scalar, zero_scalar, beta_linear, dt, h_min[i], mesh)
+    Nu_projected = [ev(J, zero_tensor, zero_tensor, beta_const, dt, h_min[i], mesh),
+                    ev(zero_tensor, J_old, zero_tensor, beta_const, dt, h_min[i], mesh),
+                    ev(zero_tensor, zero_tensor, J_old_old, beta_const, dt, h_min[i], mesh),
+                    ev(coords, zero_tensor, zero_tensor, beta_linear, dt, h_min[i], mesh)]
 
     # Nu cases
-    Nu_J = StabilizationParameter(J, zero_scalar, zero_scalar, beta_const, c_e, Nu_max, dt, E_norm, t_step)
-    Nu_J_old = StabilizationParameter(zero_scalar, J_old, zero_scalar, beta_const, c_e, Nu_max, dt, E_norm, t_step)
-    Nu_J_old_old = StabilizationParameter(zero_scalar, zero_scalar, J_old_old, beta_const, c_e, Nu_max, dt, E_norm, t_step)
-
-    Nu_beta = StabilizationParameter(coords, zero_scalar, zero_scalar, beta_linear, c_e, Nu_max, dt, E_norm, t_step)
+    Nu_computed = [StabilizationParameter(J, zero_tensor, zero_tensor, beta_const, c_e, c_max, dt, E_norm, time_step),
+                   StabilizationParameter(zero_tensor, J_old, zero_tensor, beta_const, c_e, c_max, dt, E_norm, time_step),
+                   StabilizationParameter(zero_tensor, zero_tensor, J_old_old, beta_const, c_e, c_max, dt, E_norm, time_step),
+                   StabilizationParameter(coords, zero_tensor, zero_tensor, beta_linear, c_e, c_max, dt, E_norm, time_step)]
 
     # Projection of Nu cases
-    Nu_Jh = dlfn.project(Nu_J, Jh_test)
-    Nu_J_oldh = dlfn.project(Nu_J_old, Jh_test)
-    Nu_J_old_oldh = dlfn.project(Nu_J_old_old, Jh_test)
-
-    Nu_betah = dlfn.project(Nu_beta, Jh_test)
+    Nu_computed_projected = [dlfn.project(nu, Jh_test) for nu in Nu_computed]
 
     J_list = []
     J_old_list = []
     J_old_old_list = []
     beta_list = []
 
+    # Iterate over cells in given mesh: will not work in parallel and is only supposed for validating stabilization.py
     for cell in dlfn.cells(mesh):
         x_i = np.mean(np.reshape(cell.get_coordinate_dofs(), (-1, dim)), axis=0)
         vals = np.zeros(1)
 
-        Nu_Jh.eval(vals, x_i)
-        J_list.append(abs(vals[0]-maximum(Nu_J_projected, cell, dim))/(abs(vals[0])))
-        assert abs(vals[0] - maximum(Nu_J_projected, cell, dim)) / abs(vals[0]) < .1
+        Nu_computed_projected[0].eval(vals, x_i)
+        J_list.append(abs(vals[0]-maximum(Nu_projected[0], cell, dim))/(abs(vals[0])))
+        assert abs(vals[0] - maximum(Nu_projected[0], cell, dim)) / abs(vals[0]) < .1
 
-        Nu_J_oldh.eval(vals, x_i)
-        J_old_list.append(abs(vals[0] - maximum(Nu_J_old_projected, cell, dim)) / abs(vals[0]))
-        assert abs(vals[0] - maximum(Nu_J_old_projected, cell, dim)) / abs(vals[0]) < .1
+        Nu_computed_projected[1].eval(vals, x_i)
+        J_old_list.append(abs(vals[0] - maximum(Nu_projected[1], cell, dim)) / abs(vals[0]))
+        assert abs(vals[0] - maximum(Nu_projected[1], cell, dim)) / abs(vals[0]) < .1
 
-        Nu_J_old_oldh.eval(vals, x_i)
-        J_old_old_list.append(abs(vals[0] - maximum(Nu_J_old_old_projected, cell, dim)) / abs(vals[0]))
-        assert abs(vals[0] - maximum(Nu_J_old_old_projected, cell, dim)) / abs(vals[0]) < .1
+        Nu_computed_projected[2].eval(vals, x_i)
+        J_old_old_list.append(abs(vals[0] - maximum(Nu_projected[2], cell, dim)) / abs(vals[0]))
+        assert abs(vals[0] - maximum(Nu_projected[2], cell, dim)) / abs(vals[0]) < .1
 
-        Nu_betah.eval(vals, x_i)
-        beta_list.append(abs(vals[0] - maximum(Nu_beta_projected, cell, dim)) / abs(vals[0]))
-        assert abs(vals[0] - maximum(Nu_beta_projected, cell, dim)) / abs(vals[0]) < .1
+        Nu_computed_projected[3].eval(vals, x_i)
+        beta_list.append(abs(vals[0] - maximum(Nu_projected[3], cell, dim)) / abs(vals[0]))
+        assert abs(vals[0] - maximum(Nu_projected[3], cell, dim)) / abs(vals[0]) < .1
 
     median_arr[i][0] = np.median(J_list)
     median_arr[i][1] = np.median(J_old_list)
@@ -163,21 +162,35 @@ for i, mesh in enumerate(meshes):
 print("Validation successful!")
 
 # Plot relative error
-fig, ax = plt.subplots()
-im = ax.imshow(median_arr, cmap='inferno', interpolation='nearest')
+fig, ax = plt.subplots(1, 2, figsize=(10, 4))
+
+im1 = ax[0].imshow(median_arr, cmap='inferno', interpolation='nearest')
+im2 = ax[1].imshow(var_arr, cmap='inferno', interpolation='nearest')
 
 
-ax.set_yticks(np.arange(3))
-ax.set_yticklabels(["1D", "2D", "3D"])
+ax[0].set_yticks(np.arange(3))
+ax[0].set_yticklabels(["1D", "2D", "3D"])
 
-ax.set_xticks(np.arange(4))
-ax.set_xticklabels(["J", "J_old", "J_old_old", "beta"])
+ax[0].set_xticks(np.arange(4))
+ax[0].set_xticklabels(["J", "J_old", "J_old_old", "beta"])
+
+ax[0].title.set_text('Median')
+
+ax[1].set_yticks(np.arange(3))
+ax[1].set_yticklabels(["1D", "2D", "3D"])
+
+ax[1].set_xticks(np.arange(4))
+ax[1].set_xticklabels(["J", "J_old", "J_old_old", "beta"])
+
+ax[1].title.set_text('Variance')
 
 textcolors = ("black", "white")
-threshold = np.median(median_arr)
+threshold = (np.mean(median_arr), np.mean(var_arr))
 for i in range(3):
     for j in range(4):
-        text = ax.text(j, i, "{:.2e}".format(median_arr[i, j]),
-                       ha="center", va="center", color=textcolors[median_arr[i, j] < threshold])
+        text1 = ax[0].text(j, i, "{:.2e}".format(median_arr[i, j]),
+                           ha="center", va="center", color=textcolors[median_arr[i, j] < threshold[0]])
+        text2 = ax[1].text(j, i, "{:.2e}".format(var_arr[i, j]),
+                           ha="center", va="center", color=textcolors[var_arr[i, j] < threshold[1]])
 
 plt.savefig('results/rel_err.png')
