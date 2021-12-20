ba23b41b8ea0e26350f9461ccd2be54811067aac
massingf <f.massing@campus.tu-berlin.de>
2021-12-20 13:16:00 +0100

    Only pass c_max to stabilization.py, as nu_max is computed in there.

--- scripts/crusher_1D.py
+++ scripts/crusher_1D.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 p_deg = 2
 
 # Domain
-nx = 200	# discretization points in x-direction
+nx = 200  # discretization points in x-direction
 h_k = 1. / nx
 
 # Transport Velocity
@@ -27,7 +27,7 @@ dt_0 = dt
 time_step = dlfn.Constant(0)
 
 # E-V-Parameters
-c_e = 2. # Let c_e -> infty to find c_max
+c_e = 2.  # Let c_e -> infty to find c_max
 c_max = .3
 
 # crusher parameters
@@ -100,6 +100,7 @@ dlfn.assign(u, dlfn.project(dlfn.Expression('u_0', u_0=u_0, degree=2), Vh))
 sol_ = dlfn.Function(Wh)
 sol_n = dlfn.Function(Wh)
 sol_n_ = dlfn.Function(Wh)
+
 
 def F_spatial_visc_chi(sol_, del_, beta, Nu, chi_h):
     return F_spatial(sol_, del_, beta) + Nu * Dx(sol_, 0) * Dx(del_, 0) - chi_h * del_
@@ -181,37 +182,35 @@ dlfn.assign(sol_h.sub(0), rho_initial)
 dlfn.assign(sol_h.sub(1), J_initial)
 
 # == Stabilization =====================
-Nu_max = c_max * h_k * abs(u_0)
-
 E_rho_norm = dlfn.Constant(calculate_NormE(sol_h.sub(0), n_dofs))
 E_J_norm = dlfn.Constant(calculate_NormE(sol_h.sub(1), n_dofs))
 
-Nu_h_rho = StabilizationParameter(sol_h.sub(0), sol_n.sub(0), sol_n_.sub(0), u, c_e, Nu_max, dlfn.Constant(dt), E_rho_norm, time_step)
-Nu_h_J = StabilizationParameter(sol_h.sub(1), sol_n.sub(1), sol_n_.sub(1), u, c_e, Nu_max, dlfn.Constant(dt), E_J_norm, time_step)
+Nu_h_rho = StabilizationParameter(sol_h.sub(0), sol_n.sub(0), sol_n_.sub(0), u, c_e, c_max, dlfn.Constant(dt), E_rho_norm, time_step)
+Nu_h_J = StabilizationParameter(sol_h.sub(1), sol_n.sub(1), sol_n_.sub(1), u, c_e, c_max, dlfn.Constant(dt), E_J_norm, time_step)
 
 # == Problem in variational Form =======
+# The fields rho and J are computed as a 'coupled system', although only J depends on rho and rho could is solved
+# analytically by a Heaviside-function.
+
 F_t_rho = (sol_rho - sol_rho_h) * del_rho * dx
 F_t_J = (sol_J - sol_J_h) * del_J * dx
 
 F_rho_1 = (ki_rho * del_rho + F_spatial_visc(sol_h.sub(0), del_rho, u, Nu_h_rho)) * dx
 F_J_1 = (ki_J * del_J + F_spatial_visc_chi(sol_h.sub(1), del_J, u, Nu_h_J, chi_h)) * dx
-F_1 = F_rho_1 + F_J_1
 
 F_rho_2 = (ki_rho * del_rho + F_spatial_visc(sol_h.sub(0) + .5 * dT * kh_rho[0], del_rho, u, Nu_h_rho)) * dx
 F_J_2 = (ki_J * del_J + F_spatial_visc_chi(sol_h.sub(1) + .5 * dT * kh_J[0], del_J, u, Nu_h_J, chi_h)) * dx
-F_2 = F_rho_2 + F_J_2
 
 F_rho_3 = (ki_rho * del_rho + F_spatial_visc(sol_h.sub(0) + .5 * dT * kh_rho[1], del_rho, u, Nu_h_rho)) * dx
 F_J_3 = (ki_J * del_J + F_spatial_visc_chi(sol_h.sub(1) + .5 * dT * kh_J[1], del_J, u, Nu_h_J, chi_h)) * dx
-F_3 = F_rho_3 + F_J_3
 
 F_rho_4 = (ki_rho * del_rho + F_spatial_visc(sol_h.sub(0) + dT * kh_rho[2], del_rho, u, Nu_h_rho)) * dx
 F_J_4 = (ki_J * del_J + F_spatial_visc_chi(sol_h.sub(1) + dT * kh_J[2], del_J, u, Nu_h_J, chi_h)) * dx
-F_4 = F_rho_4 + F_J_4
 
 F_rho = F_t_rho - dT / 3. * (.5 * kh_rho[0] + kh_rho[1] + kh_rho[2] + .5 * kh_rho[3]) * del_rho * dx
 F_J = F_t_J - dT / 3. * (.5 * kh_J[0] + kh_J[1] + kh_J[2] + .5 * kh_J[3]) * del_J * dx
 
+F_rec = [F_rho_1 + F_J_1, F_rho_2 + F_J_2, F_rho_3 + F_J_3, F_rho_4 + F_J_4]
 F = F_rho + F_J
 
 # boundary subdomains
@@ -236,7 +235,6 @@ bcs.append(dlfn.DirichletBC(Jh, dlfn.Constant(0.), facet_marker, 2))
 t_i, i = 0., 0
 while t_i < t_end:
     print("time step {i}, time {t_i}".format(i=i, t_i=t_i))
-    #dT.assign(dt * np.tanh((i + 1) / t_adaptive))
     t_i += dT.values()[0]
     print(t_i)
     time_step.assign(i)
@@ -244,15 +242,12 @@ while t_i < t_end:
     E_rho_norm.assign(calculate_NormE(sol_h.sub(0), n_dofs))
     E_J_norm.assign(calculate_NormE(sol_h.sub(1), n_dofs))
 
-
-
-    dlfn.solve(lhs(F_1) == rhs(F_1), kh[0])
-    dlfn.solve(lhs(F_2) == rhs(F_2), kh[1])
-    dlfn.solve(lhs(F_3) == rhs(F_3), kh[2])
-    dlfn.solve(lhs(F_4) == rhs(F_4), kh[3])
+    dlfn.solve(lhs(F_rec[0]) == rhs(F_rec[0]), kh[0])
+    dlfn.solve(lhs(F_rec[1]) == rhs(F_rec[1]), kh[1])
+    dlfn.solve(lhs(F_rec[2]) == rhs(F_rec[2]), kh[2])
+    dlfn.solve(lhs(F_rec[3]) == rhs(F_rec[3]), kh[3])
 
     dlfn.solve(lhs(F) == rhs(F), sol_h, bcs=bcs)
-
 
     # === Assign to Buffer Functions =============
     dlfn.assign(sol_n_.sub(0), sol_n.split()[0])
@@ -263,7 +258,6 @@ while t_i < t_end:
 
     dlfn.assign(sol_.sub(0), sol_h.split()[0])
     dlfn.assign(sol_.sub(1), sol_h.split()[1])
-
 
     # Save as .vtk
     mod_i = 10
@@ -295,6 +289,5 @@ while t_i < t_end:
         plt.legend(['J_ana', 'J'])
         plt.savefig(os.path.join(path, 'img/sol_{}.png'.format((i+1) // mod_i)))
         plt.clf()
-
 
     i += 1
